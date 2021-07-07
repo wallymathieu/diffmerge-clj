507334c4b2223b9cf5ad0de796c7a911810d3527
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2021-07-07 07:53:06 +0200

    diff

--- src/diffmerge/core.clj
+++ src/diffmerge/core.clj
@@ -1,6 +1,18 @@
-(ns diffmerge.core)
+(ns diffmerge.core
+  (:require
+    [clojure.set :as cset]
+    ))
 
+(defn- set-contains [set] (fn [[key val]] (contains? set key)))
 
-(defn foo "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn diff "symmetric diff"
+  [left right]
+  (let [set-left (-> left keys set)
+        set-right (-> right keys set) 
+        only-on-left (into {} (filter (set-contains (cset/difference set-left set-right)) left))
+        only-on-right (into {} (filter (set-contains (cset/difference set-right set-left)) right)) 
+
+        intersection (into {} (map (fn [k] [k [(get left k) (get right k)]]) (cset/intersection set-right set-left)))
+       ]
+    {:only-in-right only-on-right :only-in-left only-on-left :intersection intersection} 
+  ))
--- test/diffmerge/core_test.clj
+++ test/diffmerge/core_test.clj
@@ -3,6 +3,16 @@
             [diffmerge.core :refer :all]))
 
 
-(deftest a-test
-  (testing "FIXME, I don't fail."
-    (is (true? (= 4 (count (str (= 1 1))))))))
+(deftest given-two-maps
+  (is (= {:only-in-right {:c 4}, :only-in-left {:a 1, :d 5}, :intersection {:b [2 3]}} 
+           (diff {:a 1 :b 2 :d 5} {:b 3 :c 4}))))
+
+(deftest when-there-is-an-incoming-element 
+  (is (= {:only-in-right {2 2}, :only-in-left {}, :intersection {1 ["1" 1]}} 
+    (diff {1 "1"} {1 1 2 2}))))
+(deftest when-there-an-empty-incoming-list 
+  (is (= {:only-in-right {}, :only-in-left {1 "1"}, :intersection {}} 
+    (diff {1 "1"} {}))))
+(deftest when-incoming-is-the-same-as-existing 
+  (is (= {:only-in-right {}, :only-in-left {}, :intersection {1 ["1" 1]}} 
+    (diff {1 "1"} {1 1}))))

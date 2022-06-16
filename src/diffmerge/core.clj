cf727b4a58bee7f10af2461bf0c554bed76908b6
Carlo Sciolla <carlo.sciolla@gmail.com>
2022-06-16 10:00:30 +0200

    :gear: Streamline code
    During our chat at the Dutc clojure meetup, this code was produced
    as a refactoring of your initial version
    
    Hope this helps

--- src/diffmerge/core.clj
+++ src/diffmerge/core.clj
@@ -1,18 +1,12 @@
-(ns diffmerge.core
-  (:require
-    [clojure.set :as cset]
-    ))
-
-(defn- set-contains [set] (fn [[key val]] (contains? set key)))
+(ns diffmerge.core)
 
 (defn diff "symmetric diff"
   [left right]
-  (let [set-left (-> left keys set)
-        set-right (-> right keys set) 
-        only-on-left (into {} (filter (set-contains (cset/difference set-left set-right)) left))
-        only-on-right (into {} (filter (set-contains (cset/difference set-right set-left)) right)) 
-
-        intersection (into {} (map (fn [k] [k [(get left k) (get right k)]]) (cset/intersection set-right set-left)))
-       ]
-    {:only-in-right only-on-right :only-in-left only-on-left :intersection intersection} 
-  ))
+  (let [common-keys (keys (select-keys left (keys right)))
+        only-on-left (apply dissoc left (keys right))
+        only-on-right (apply dissoc right (keys left))
+        intersection (into {} (for [k common-keys]
+                                [k [(get left k) (get right k)]]))]
+    {:only-in-right only-on-right
+     :only-in-left only-on-left
+     :intersection intersection}))
